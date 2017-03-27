c0475926f6927fc7917a7ae93703765b3faef25e
neonua <neonua666@gmail.com>
2017-03-27 18:29:10 +0300

    secong push

--- shop_app/views.py
+++ shop_app/views.py
@@ -3,11 +3,11 @@ from django.views.generic.list import ListView, View
 from django.views.generic import TemplateView, DetailView, CreateView
 from models import Product
 from braces.views import JSONResponseMixin, AjaxResponseMixin
-from django.http import Http404, HttpResponseRedirect, HttpResponseBadRequest
 from django.views.decorators.http import require_http_methods
+from .mixins import SessionGet
 
 
-class ProductList(ListView):
+class ProductList(SessionGet, ListView):
     model = Product
     template_name = 'index.html'
 
@@ -15,56 +15,37 @@ class ProductList(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(ProductList, self).get_context_data()
-        session = self.request.session.get('cart', {})
-        self.request.session['cart'] = session
         return context
 
 
-class CartList(TemplateView):
+class CartList(SessionGet, TemplateView):
 
     template_name = 'cart.html'
 
     def get_context_data(self, **kwargs):
         context = super(CartList, self).get_context_data()
-        cart = self.request.session['cart']
-        context['cart_list'] = cart
-
+        context['cart_list'] = self.cart
         return context
 
 
-class CheckID(CreateView):
-    """
-    Mixin for getting id current object
-    """
+class CartAdd(SessionGet, JSONResponseMixin, AjaxResponseMixin, DetailView):
+
     model = Product
     pk_url_kwarg = 'id'
-
-    def get_object(self, *args, **kwargs):
-        try:
-            product = super(CheckID, self).get_object(*args, **kwargs)
-        except:
-            raise Http404()
-        else:
-            return product
-
-    @require_http_methods(['POST'])
-    def dispatch(self, request, *args, **kwargs):
-
-        return super(CheckID, self).dispatch(request, *args, **kwargs)
-
-
-class CartAdd(JSONResponseMixin, AjaxResponseMixin, CheckID):
 
     def post_ajax(self, request, *args, **kwargs):
         product = self.get_object()
         product_id = str(product.id)
-        cart = self.request.session.get('cart', {})
 
-        if product_id in cart:
-            cart[product_id] += 1
+        if product_id in self.cart:
+            self.cart[product_id] += 1
         else:
-            cart[product_id] = 1
-        self.request.session['cart'] = cart
+            self.cart[product_id] = 1
+        self.request.session['cart'] = self.cart
 
-        return self.render_json_response(cart)
+        return self.render_json_response(self.cart)
 
+    # @require_http_methods(['POST'])
+    # def dispatch(self, request, *args, **kwargs):
+    #
+    #     return super(CartAdd, self).dispatch(request, *args, **kwargs)
